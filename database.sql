e89d69e57fb66df4d3411f061b6114b7b2bfa9ca
nhan 295 <tranthihongnhan295@gmail.com>
2024-11-03 16:16:44 +0700

    update new database

--- database.sql
+++ database.sql
@@ -1,50 +1,43 @@
 CREATE DATABASE BOOKS;
 USE BOOKS;
 drop database books;
-
-CREATE TABLE NGUOI_DUNG(
-	 ND_ID INT AUTO_INCREMENT PRIMARY KEY,
-	 ND_MatKhau VARCHAR(255),
-	 ND_HoTen CHAR(30),
-	 ND_GioiTinh enum('Nam','Nữ'),
-	 ND_NgayTao DATE,
-	 ND_Email varchar(50) check (`ND_Email` regexp "^[a-zA-Z0-9][a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]*?[a-zA-Z0-9._-]?@[a-zA-Z0-9][a-zA-Z0-9._-]*?[a-zA-Z0-9]?\\.[a-zA-Z]{2,63}$"),
-	 ND_SoDienThoai char(10) check(regexp_replace(ND_SoDienThoai,'[^0-9]', '')),
-	 ND_DiaChi VARCHAR(50),
-     ND_VaiTro CHAR(20)
+drop table NGUOI_DUNG;
+CREATE TABLE USERS(
+	 user_id INT AUTO_INCREMENT PRIMARY KEY,
+	 user_password VARCHAR(255),
+	 user_name CHAR(30),
+	 user_gender enum('Nam','Nữ'),
+	 date_created date,
+	 user_email varchar(50) check (`user_Email` regexp "^[a-zA-Z0-9][a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]*?[a-zA-Z0-9._-]?@[a-zA-Z0-9][a-zA-Z0-9._-]*?[a-zA-Z0-9]?\\.[a-zA-Z]{2,63}$"),
+	 user_phone char(10) check(regexp_replace(user_phone,'[^0-9]', '')),
+	 user_address VARCHAR(50),
+     user_avatar varchar(255) default 'default.jpg'
      );
 
- CREATE TABLE LOAI_SACH(
-	LS_ID INT AUTO_INCREMENT PRIMARY KEY,
-	LS_Ten CHAR(30),
-	LS_MoTa VARCHAR(50)
+ CREATE TABLE BOOK_TYPES(
+	type_id INT AUTO_INCREMENT PRIMARY KEY,
+	type_name CHAR(30),
+	type_detail VARCHAR(50)
 );
-
-
-CREATE TABLE SACH(
-	SACH_ID INT AUTO_INCREMENT PRIMARY KEY,
-	SACH_Ten CHAR(30),
-    SACH_Anh longblob,
-    SACH_TacGia CHAR(30),
-	SACH_NgonNgu CHAR(30),
-	SACH_SoLuong INT,
-	SACH_GiaBan FLOAT(6,3),
-	SACH_NhaXuatBan CHAR(30),
-	SACH_NgayXuatBan DATE,
-	SACH_Sotrang INT,
-	SACH_MoTa VARCHAR(300),
-	LS_ID INT,
-	FOREIGN KEY (LS_ID) REFERENCES LOAI_SACH(LS_ID)
+select * from book_types;
+insert book_types (type_name) value('Economic');
+CREATE TABLE BOOK(
+	book_id INT AUTO_INCREMENT PRIMARY KEY,
+	book_title CHAR(50),
+    book_image varchar(255) default 'default.jpg',
+    book_author CHAR(30),
+	book_language CHAR(30),
+	book_stock INT,
+	book_price FLOAT(6,3),
+	book_discount INT,
+	book_publisher CHAR(30),
+	published_date DATE,
+	total_pages INT,
+	book_detail VARCHAR(500),
+	type_id INT,
+	FOREIGN KEY (type_id) REFERENCES BOOK_TYPES(type_id)
 );
-select * from sach;
-
-CREATE TABLE GIO_HANG(
-	GH_ID INT AUTO_INCREMENT PRIMARY KEY,
-	ND_ID INT,
-	SACH_ID INT,
-	GH_SoLuong INT,
-	FOREIGN KEY (ND_ID) REFERENCES NGUOI_DUNG(ND_ID),
-	FOREIGN KEY (SACH_ID) REFERENCES SACH(SACH_ID)
-	);
-
+drop table book;
+select * from book;
+insert into book(book_title,book_image,book_author,book_language,book_publisher,book_price,book_discount,total_pages,book_detail,type_id) value('Khám phá Mỹ Học','./assets/adven3.webp','Mautice Nédoncelle','Tiếng Việt','Tri Thức','95.000','73.000','228','Cuốn sách không chỉ là một hành trình qua các khía cạnh của nghệ thuật, mà còn là một phiêu lưu cá nhân trong thế giới thẩm mĩ. Từ việc khám phá nguồn gốc của nghệ thuật đến việc suy ngẫm về trạng thái cuối cùng của mọi thứ, độc giả sẽ được dẫn dắt qua những con đường lớn của vũ trụ nghệ thuật.',2);
 
